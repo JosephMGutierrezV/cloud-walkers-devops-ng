5c75f88e7efbd7d0bd38ad55bc54aa26c2db5d9d
Joseph Gutierrez <josephmauricio23@hotmail.com>
2023-03-11 08:17:31 -0500

    Box shadow with grow

--- projects/cloud-walkers-devops-ng/src/lib/components/youtube-player/youtube-player.component.ts
+++ projects/cloud-walkers-devops-ng/src/lib/components/youtube-player/youtube-player.component.ts
@@ -1,39 +1,58 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { UniqueComponentId } from '../../utils/unique-id.utils';
 
 export const DEFAULT_PLAYER_WIDTH = 640;
 export const DEFAULT_PLAYER_HEIGHT = 390;
 
+
+//TODO: Add support multiple players
 @Component({
   selector: 'c-youtube-player',
   templateUrl: './youtube-player.component.html',
   styleUrls: ['./youtube-player.component.scss'],
 })
-export class CYoutubePlayerComponent implements OnInit {
+export class CYoutubePlayerComponent implements OnInit, AfterViewInit {
   @Input() videoId: string = '';
   @Input() width: number = DEFAULT_PLAYER_WIDTH;
   @Input() height: number = DEFAULT_PLAYER_HEIGHT;
-  private player: any;
+  @Input() autoplay: boolean = false;
+  @Input() start: number = 0;
+  @Input() end: number = 0;
 
-  constructor() {}
+  private player: any;
+  public uniqueId: string = '';
+
+  constructor() {
+    this.uniqueId = UniqueComponentId();
+    if (!this.isYouTubeScriptLoaded()) {
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+    }
+  }
 
   ngOnInit() {
-    const tag = document.createElement('script');
-    tag.src = 'https://www.youtube.com/iframe_api';
-    document.body.appendChild(tag);
-
     const objectWindow = window as any;
-
     objectWindow['onYouTubeIframeAPIReady'] = () => {
-      this.player = new objectWindow['YT'].Player('c-player', {
+      this.player = new objectWindow['YT'].Player(this.uniqueId, {
         height: this.height,
-        width: this.width,
         videoId: this.videoId,
+        width: this.width,
+        autoplay: this.autoplay,
+        startSeconds: this.start,
+        endSeconds: this.end,
         events: {
           onReady: this.onPlayerReady,
           onStateChange: this.onPlayerStateChange,
         },
       });
     };
+  }
+
+  ngAfterViewInit(): void {
+    if (this?.player) {
+      this.player.loadVideoById(this.videoId);
+    }
   }
 
   onPlayerReady(event: any) {
@@ -43,7 +62,17 @@ export class CYoutubePlayerComponent implements OnInit {
   onPlayerStateChange(event: any) {
     const objectWindow = window as any;
     if (event.data === objectWindow['YT'].PlayerState.PLAYING) {
-      this.player.stopVideo();
+      if (this?.player) {
+        this.player.stopVideo();
+      }
     }
+  }
+
+  private isYouTubeScriptLoaded(): boolean {
+    return (
+      document.querySelector(
+        'script[src="https://www.youtube.com/iframe_api"]'
+      ) !== null
+    );
   }
 }
